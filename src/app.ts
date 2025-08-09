f519a3ce052c6699bc7321474ee66ed13e419c83
rashedrk <rashedrk001@gmail.com>
2025-08-10 00:58:10 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to EForge Quiz App Server');
 });
 
+app.use(globalErrorHandler)
 
 export default app;

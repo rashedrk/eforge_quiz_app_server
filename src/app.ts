2abb19ed2f45542e4a7c1e8f63d0d94d15d952fd
rashedrk <rashedrk001@gmail.com>
2025-08-11 00:10:05 +0600

    origin add to cors

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import router from "./app/routes";
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', "https://eforge-quiz-app-client.vercel.app"], credentials: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to EForge Quiz App Server');
